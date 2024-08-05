40f39a7e85f0426d9c9f5afe07dee67c137d8756
LeeJeongbin <ljb97310@naver.com>
2024-08-05 16:50:51 +0900

    두 수의 곱

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace problem001
+{
+    internal class Program
+    {
+        public int solution(int num1, int num2)
+        {
+            int answer = num1 * num2;
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
