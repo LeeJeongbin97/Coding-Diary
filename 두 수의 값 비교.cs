cabb5e2f77f8958e44e0843bf3f287909ff7c744
LeeJeongbin <ljb97310@naver.com>
2024-08-06 12:03:16 +0900

    두 수의 값 비교

---
+++ 두 수의 값 비교.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Solution
+{
+    // 두 수의 값 비교
+    public int solution(int num1, int num2)
+    {
+        if (num1 != num2) // num1과 num2가 같지 않다면
+            return -1; // -1 반환
+        else // 아니라면
+            return 1; // 1 반환
+    }
+}
