0796c06d89e2848720b10f185f006de06fbff19a
LeeJeongbin <ljb97310@naver.com>
2024-08-06 11:59:46 +0900

    몫 구하기

--- 몫 구하기.cs
+++ 몫 구하기.cs
@@ -5,7 +5,7 @@ public class Solution
     // 몫 구하기
     public int solution(int num1, int num2)
     {
-        int answer = num1 / num2;
-        return answer;
+        int answer = num1 / num2; // num1 나누기 num2 = int형 answer
+        return answer; // answer 값 반환
     }
 }
